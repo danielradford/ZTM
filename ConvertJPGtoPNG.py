3236d10dc123247bf0e21b49319c78d7ad01b19b
Daniel Radford <92265717+danielradford@users.noreply.github.com>
2021-10-12 11:34:26 +0100

    Converts JPG to PNG
    Picture Converter

--- ConvertJPGtoPNG.py
+++ ConvertJPGtoPNG.py
@@ -1,1 +1,25 @@
-import sysimport osfrom PIL import Imageimage_folder = sys.argv[1]out_folder = sys.argv[2]if not os.path.exists(out_folder):    os.makedirs(out_folder)    ext=['jpg']for filename in os.listdir(image_folder):    print(filename)    if filename[-3:] in ext:        img = Image.open(f"{image_folder}{filename}")        clean_name = os.path.splitext(filename)[0]        img.save(f"{out_folder}{clean_name}.png",'png')        print("all done")                    
+import sys
+import os
+from PIL import Image
+
+image_folder = sys.argv[1]
+out_folder = sys.argv[2]
+
+if not os.path.exists(out_folder):
+    os.makedirs(out_folder)
+
+
+    
+ext=['jpg']
+for filename in os.listdir(image_folder):
+    print(filename)
+    if filename[-3:] in ext:
+        img = Image.open(f"{image_folder}{filename}")
+        clean_name = os.path.splitext(filename)[0]
+        img.save(f"{out_folder}{clean_name}.png",'png')
+        print("All done")
+        
+    
+    
+    
+
